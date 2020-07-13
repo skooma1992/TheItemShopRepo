f09797c536b09d76f85da8abe4b5f8bcb4d96e4a
Jdowdy <Jdowdy9k@gamil.com>
2020-07-13 17:18:56 -0400

    added buttons/more style to Homepage

--- theitemshopfrontend/src/App.js
+++ theitemshopfrontend/src/App.js
@@ -1,107 +1,77 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
-<<<<<<< HEAD
 import Search from './actions/Search';
-=======
 import SigninScreen from './screens/SigninScreen';
 import { useSelector } from 'react-redux';
 import RegisterScreen from './screens/RegisterScreen'
->>>>>>> af776f91ca8baccdb98a20d20ab62e122875a127
 
 
 function App() {
 
 
-const userSignin = useSelector(state=>state.userSignin);
-const {userInfo} = userSignin
-
-  const openMenu = () =>{
-  document.querySelector(".sidebar").classList.add("open")
-  }
-  const closeMenu = () =>{
-    document.querySelector(".sidebar").classList.remove("open")
-  }
-  
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin
 
   return (
-<BrowserRouter>
-  <div className="grid-container">
+    <BrowserRouter>
+      <div className="grid-container">
 
-  <header className="header">
-      <div className="brand">
-          <button onClick={openMenu}>
-              &#9776;
-          </button>
-          <Link to = "/">The Item Shop</Link>
-          
-      </div>
-      <section className="searchbox-wrap">
-            <input type="text" 
-            placeholder="Search for an Item..." 
-            className="searchbox" 
+        <header className="header">
+          <div className="brand">
+
+            <Link to="/"><img src="/images/item-shop-logo.png"></img></Link>
+
+          </div>
+          <div className="searchbox-wrap">
+            <input type="text"
+              placeholder="Search for an Item..."
+              className="searchbox"
             //onChange= {handleInput}
             //onKeyPress= {search} 
             />
-      </section>
+            
+          </div>
 
-      <section className="headerLinks">
-        <button className="signInbutt"><a href="signin.html">Sign in</a></button>
-        <a href="cart.html"><img src="/images/bit-13-512.png" alt="cart icon"></img></a>
-      </section>
-      
-      <div className="header-links">
-          
-            {/* IF THE USER IS SIGNED IN SHOW PROFILE PAGE IF NOT SHOW SIGN IN SCREEN */}
-            {
-              userInfo ? <Link to="/profile">{userInfo.name}</Link>:
-              <Link to="/signin">Sign In</Link>
-            }
-          <a href="cart.html">Your Cart</a>
-      </div>
-  </header>
-  
- 
+          <div className="headerLinks">
+            <button className="signInbutt">
+              {/* IF THE USER IS SIGNED IN SHOW PROFILE PAGE IF NOT SHOW SIGN IN SCREEN */}
+              {
+                userInfo ? <Link to="/profile" className="userButt">{userInfo.name}</Link> :
+                  <Link to="/signin" className="signButt">Sign In</Link>
+              }
+            </button>
+            <button className="cartButt">
+              <a href="cart.html"><img src="/images/bit-13-512.png" alt="cart icon"></img></a>
+            </button>
+          </div>
+        </header>
 
-  <aside className="sidebar">
-      <h3>Shopping Categories</h3>
-      <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-      <ul>
-          <li>
-              <a href="index.html">Item category1</a>
-          </li>
-          <li>
-              <a href="index.html">Item category2</a>
-          </li>
-      </ul>
-  </aside>
+        <main className="main">
+          <div className="itemHeader">
+            <div className="itemHeader-links">
+              <button className="itembarIcon-1"><a href="index.html">Products 1</a></button>
+              <button className="itembarIcon-2"><a href="index.html">Products 2</a></button>
+            </div>
+          </div>
+          <div className="content">
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
 
-  
-  
-  <main className="main"> 
-      <div className="itemHeader">
-        <div className="itemHeader-links">
-            <button className="itembarIcon-1"><a href="index.html">Products 1</a></button>
-            <button className="itembarIcon-2"><a href="index.html">Products 2</a></button>
-        </div>
+          </div>
+
+
+        </main>
+        <footer className="footer">All Rights Reserved</footer>
       </div>
-      <div className="content">
-        <Route path ="/signin" component = {SigninScreen} />
-        <Route path = '/register' component = {RegisterScreen}/>
-        <Route path="/product/:id" component={ProductScreen} />
-        <Route path="/cart/:id?" component = {CartScreen}/>
-        <Route path="/" exact={true} component={HomeScreen} />
-          
-      </div>
-    
-  </main>
-  <footer className="footer">All Rights Reserved</footer>
-</div>
-</BrowserRouter>
-  
+    </BrowserRouter>
+
   );
 }
 
