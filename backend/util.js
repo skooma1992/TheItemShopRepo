cd34833b50ca620f8fb581e3ac0ada0fe22c20a3
skooma1992 <upsonjake@gmail.com>
2020-07-14 18:35:23 -0400

    adding checkout

--- backend/util.js
+++ backend/util.js
@@ -15,26 +15,32 @@ const getToken = (user) => {
     })
 }
 
-const isAuth = (req, res , next ) => {
+const isAuth = (req, res, next) => {
     const token = req.headers.authorization;
-    if(token){
+
+    if (token) {
         const onlyToken = token.slice(7, token.length);
-        jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) =>{
-            if (err){
-                return res.status(401).send({msg: 'Invalid Token'})
+        jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+            if (err) {
+                return res.status(401).send({ msg: 'Invalid Token' });
             }
-            req.user = token;
+            req.user = decode;
             next();
             return
-        })
+        });
+    } else {
+        return res.status(401).send({ msg: "Token is not supplied." });
     }
-    return res.status(401).send({msg: "Token is not supplied"})
-}
-const isAdmin = (req, res , next ) =>{
-    if(req.user && req.user.isAdmin){
-        return next();
-    }
-    return res.status(401).send({msg: "Admin  token is not valid"})
 }
 
-export { getToken, isAuth, isAdmin }
+const isAdmin = (req, res, next) => {
+    console.log(req.user)
+    if (req.user && req.user.isAdmin) {
+        return next();
+    }
+    return res.status(401).send({ msg: 'Admin Token is not valid.' })
+}
+
+export {
+    getToken, isAuth, isAdmin
+}
