8d31cc2dc377c995d168c64688920a9e19dc623c
Jason Coleman <jayemcee121@gmail.com>
2020-07-23 13:49:50 -0400

    fish card adjustments

--- theitemshopfrontend/src/components/fishCard.js
+++ theitemshopfrontend/src/components/fishCard.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function FishCard(props) {
     return (
         <div style={{margin: "10px"}}>
-        <Link to={"/fish/" + props.id}><Card style={{ width: '18rem' }} key={props.id}>
+        <Link to={"/fish/" + props.id}><Card style={{ width: '18rem', backgroundColor: "rgba(36, 250, 36, 0.349)" }} key={props.id}>
             <Card.Img variant="top" src={props.icon_uri} alt="fish" />
             <Card.Body>
                 <Card.Title>{props.name["name-USen"]}</Card.Title>
