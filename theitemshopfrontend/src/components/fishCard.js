0c440126010d6d8709b4dcff92cc4f1f79046803
Jason Coleman <jayemcee121@gmail.com>
2020-07-23 13:52:26 -0400

    pre push commit

--- theitemshopfrontend/src/components/fishCard.js
+++ theitemshopfrontend/src/components/fishCard.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 
 export default function FishCard(props) {
     return (
-        <div style={{margin: "10px"}}>
-        <Link to={"/fish/" + props.id}><Card style={{ width: '18rem', backgroundColor: "rgba(36, 250, 36, 0.349)" }} key={props.id}>
-            <Card.Img variant="top" src={props.icon_uri} alt="fish" />
-            <Card.Body>
-                <Card.Title>{props.name["name-USen"]}</Card.Title>
-                <Card.Text>
-                    Rarity: {props.rarity}
-                    <br />
-                                Price: ${props.price}
-                </Card.Text>
-            </Card.Body>
-        </Card ></Link>
+        <div style={{ margin: "10px" }}>
+            <Link to={"/fish/" + props.id}><Card style={{ width: '18rem', backgroundColor: "rgba(36, 250, 36, 0.349)" }} key={props.id}>
+                <Card.Img variant="top" src={props.icon_uri} alt="fish" />
+                <Card.Body>
+                    <Card.Title>{props.name["name-USen"]}</Card.Title>
+                    <Card.Text>
+                        Rarity: {props.rarity}
+                        <br />
+                        Price: ${props.price}
+                    </Card.Text>
+                </Card.Body>
+            </Card ></Link>
         </div>
     )
 
